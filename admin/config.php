2cfc9cd486a0d1f32d51644d762c9ce98d06b0b3
mailtoshowbox <mailtoshowbox@yahoo.com>
2021-11-29 18:30:59 +0530

    initial commmoit

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart-1/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart-1/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart-1/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart-1/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/MAILTOSHOWBOX/XAMPP/htdocs/opencart-1/admin/');
+define('DIR_SYSTEM', 'C:/MAILTOSHOWBOX/XAMPP/htdocs/opencart-1/system/');
+define('DIR_IMAGE', 'C:/MAILTOSHOWBOX/XAMPP/htdocs/opencart-1/image/');
+define('DIR_STORAGE', 'C:/MAILTOSHOWBOX/XAMPP/storage-y/');
+define('DIR_CATALOG', 'C:/MAILTOSHOWBOX/XAMPP/htdocs/opencart-1/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart-1');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
